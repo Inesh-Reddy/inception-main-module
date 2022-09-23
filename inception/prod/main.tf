01c494a8bebbaf7cfa5320d5aaffd2dba4710fb5
“Inesh-Reddy” <ineshreddy007@gmail.com>
2022-09-23 12:18:54 +0530

    made changes in prod

--- inception/prod/main.tf
+++ inception/prod/main.tf
@@ -8,10 +8,19 @@ resource "random_id" "name" {
 }
 
 module "vpc" {
-  source = "../../modules/vpc"
+  source = "git@github.com:Inesh-Reddy/inception-networking-module.git"
+  //source = "../../modules/vpc"
 
   cidr_block = "10.0.0.0/16"
+  availability_zones = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
 
+  vpc_cidr_block = "10.112.0.0/16"
+
+  public_cidr_block   = "10.112.0.0/20"
+  public_subnet_count = 3
+
+  private_cidr_block   = "10.112.16.0/20"
+  private_subnet_count = 3
   tags = {
     Name = random_id.name.hex
   }
@@ -20,7 +29,8 @@ module "vpc" {
 # One subnet should take the whole CIDR block:
 
 module "subnets1" {
-  source = "../../modules/private-subnets"
+  source = "git@github.com:Inesh-Reddy/inception-networking-module.git"
+  //source = "../../modules/private-subnets"
 
   vpc_id = module.vpc.vpc_id
 
@@ -35,8 +45,9 @@ module "subnets1" {
 
 # Two subnets should split the CIDR block in half.
 
-module "subnets2" {
-  source = "../../modules/private-subnets"
+/* module "subnets2" {
+  source = "git@github.com:Inesh-Reddy/inception-networking-module.git"
+  //source = "../../modules/private-subnets"
 
   vpc_id = module.vpc.vpc_id
 
@@ -47,13 +58,14 @@ module "subnets2" {
   tags = {
     Name = random_id.name.hex
   }
-}
+} */
 
 # Three subnets should split the CIDR block into quarters
 # but then only use 3 of them.
 
-module "subnets3" {
-  source = "../../modules/private-subnets"
+/* module "subnets3" {
+  source = "git@github.com:Inesh-Reddy/inception-networking-module.git"
+  //source = "../../modules/private-subnets"
 
   vpc_id = module.vpc.vpc_id
 
@@ -64,13 +76,14 @@ module "subnets3" {
   tags = {
     Name = random_id.name.hex
   }
-}
+} */
 
 # Four subnets should split the CIDR block into quarters and use all of them,
 # but with only 3 availability zones there will be 2 subnets in 1 of the zones.
 
-module "subnets4" {
-  source = "../../modules/private-subnets"
+/* module "subnets4" {
+  source = "git@github.com:Inesh-Reddy/inception-networking-module.git"
+  //source = "../../modules/private-subnets"
 
   vpc_id = module.vpc.vpc_id
 
@@ -81,4 +94,4 @@ module "subnets4" {
   tags = {
     Name = random_id.name.hex
   }
-}
+} */
